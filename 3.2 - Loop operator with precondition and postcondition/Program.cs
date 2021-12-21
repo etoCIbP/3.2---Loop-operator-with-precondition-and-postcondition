d207e8a9c16b77188ffb5a8391e9c4c741d7ba84
Teya <Teya@TVOROG>
2021-12-21 23:22:21 +0200

    Добавьте файлы проекта.

---
+++ 3.2 - Loop operator with precondition and postcondition/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _3._2___Loop_operator_with_precondition_and_postcondition
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			int n;
+
+			Console.Write("n = ");
+			n = int.Parse(Console.ReadLine());
+			for (int i = 1; i <= n; i++)
+			{
+				if (i < 10 && i == (i * i) % 10)
+				{
+					Console.Write(i + " " + i * i);
+				}
+
+			}
+		}
+    }
+}
